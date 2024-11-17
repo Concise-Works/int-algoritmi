a238f364bdfd04524897257412951f18351e4103
Christian Rudder <ringthebellpepper@gmail.com>
2024-11-16 22:03:59 -0500

    dfs

--- Implementation/alg.py
+++ Implementation/alg.py
@@ -78,6 +78,13 @@ a graph G is a tree if any two statements are true:
 - G is connected
 - G has n-1 edges
 - G has no cycles
+
+Types of Edges in a graph:
+1. Tree-edges: an edge in a BFS tree
+2. Forward-edges: a non-tree-edge connecting an ancestor to a descendant
+3. Backward-edges: descendant to ancestor
+4. Cross-edge: connects two nodes that don't have ancestor or defendant
+               connections (Does not create cycles).
 """
 
 """
@@ -103,7 +110,7 @@ def bfs(G: Dict[int, List[int]],s: int):
     visited[s] = True
 
     # Loop the queue until empty
-    while(len(q) > 0):
+    while q:
         p = q.popleft()
         T[p] = []
         # loop all connections
@@ -123,7 +130,7 @@ has 'd' connections m:= number of all 'd' connections in the table.
 Space complexity: O(n+m) the input and output graph are same length.
 """
 
-"""
+r"""
 Example G:
      2   4
     / \ /
@@ -132,10 +139,17 @@ Example G:
      1   5
 """
 
-G = {0: [2,1], 1:[0,3],2:[0,3],3:[1,2,4,5],4:[3],5:[3]}
+G = {
+    0: [2,1],
+    1:[0,3],
+    2:[0,3],
+    3:[1,2,4,5],
+    4:[3],
+    5:[3]
+    }
 
 # print(bfs(G,0))
-"""
+r"""
      2   4
     / \ /
    0   3
@@ -143,7 +157,7 @@ G = {0: [2,1], 1:[0,3],2:[0,3],3:[1,2,4,5],4:[3],5:[3]}
      1   5
 """
 # print(bfs(G,1))
-"""
+r"""
      2   4
     /   /
    0   3
@@ -151,10 +165,116 @@ G = {0: [2,1], 1:[0,3],2:[0,3],3:[1,2,4,5],4:[3],5:[3]}
      1   5
 """
 # print(bfs(G,3))
-"""
+r"""
      2   4
       \ /
    0   3
     \ / \ 
      1   5
 """
+
+"""
+Depth-First-Search:
+visit each branch fully (does not find longest path)
+method (stack):
+Process a node, then put it's children on a stack. Pop an item
+from the stack then process that node. Continue till stack is empty
+"""
+
+def dfs(G: Dict[int,List[int]],s: int):
+    n = len(G)
+    stack = []
+    T = {} # Parent table
+    visited = [False] * n
+    stack.append(s)
+    last_node = None
+    while stack:
+        # print(stack)
+        p = stack[-1]
+        if visited[p]:
+            stack.pop()
+        else:
+            visited[p] = True
+            T[p] = last_node
+            last_node = p
+            for c in G[p]:
+                if not visited[c]:
+                    stack.append(c)
+    return T
+r"""
+Time complexity: O(n+m), we traverse all nodes, every node we traverse we 
+mark as visited, and is not added to the stack
+
+Space complexity: O(n+m), for incoming Graph
+Example G:
+     2   4
+    / \ /
+   0   3
+    \ / \ 
+     1   5
+"""
+# print(dfs(G,0)) # 0-1-3-5-4-2
+
+r"""
+Stack:
+            5  5v 
+            4  4  4  4v
+            2  2  2  2  2  2v
+         3  3v 3v 3v 3v 3v 3v 3v 
+      1  1v 1v 1v 1v 1v 1v 1v 1v 1v 
+      2  2  2  2  2  2  2  2v 2v 2v 2v
+    0 0v 0  0v 0v 0v 0v 0v 0v 0v 0v 0v 0v
+    -------------------------------------
+    0 1  2  3  4  5  6  7  8  9  10 11 12
+
+Resulting traversal:
+     2---4
+         |
+   0   3 |
+    \ / \|  
+     1   5
+Which mimics the backtracking 0->1->3->5->3->4->3->2
+This works because we place on the stack what is reachable from each node
+essentially working as a bfs queue but we only explore one path at a time.
+We keep track what path/nodes have already been visited
+
+"""
+
+H = {
+    0: [2,1],
+    1: [4,3],
+    2: [5],
+    3: [],
+    4: [],
+    5: []
+}
+r"""
+      0
+     / \
+    1   2
+   / \   \
+  3   4   5
+"""        
+print(dfs(H,0)) # 0-1-3-4-2-5
+"""
+[0]
+[0v, 2, 1]
+[0v, 2, v1, 4, 3]
+[0v, 2, v1, 4, 3v]
+[0v, 2, v1, 4]
+[0v, 2, v1, 4v]
+[0v, 2, v1]
+[0v, 2]
+[0v, 2v, 5]
+[0v, 2v, 5v]
+[0v, 2v]
+[0v]
+"""
+
+
+
+
+
+        
+
+
